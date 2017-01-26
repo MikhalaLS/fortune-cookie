afb754d53e92ccbb7110dcdf4e2088cdeff26582
MikhalaLS <m.lantz.simmons@gmail.com>
2017-01-26 12:56:13 -0600

    fixed typing errors

--- main.py
+++ main.py
@@ -17,24 +17,24 @@
 import webapp2
 import random
 
-def getRandomFurtune():
+def getRandomFortune():
     fortunes = [
     "You will find a puppy soon",
     "Be on the lookout for money-saving coupons",
     "Chocolate brownies are headed your way"
     ]
-    index = random.randit(0,2)
+    index = random.randint(0,2)
     return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         header = "<h1>Fortune Cookie</h1>"
         fortune = "<strong>" + getRandomFortune() + "</strong>"
-        fortune_sentence = "Your fortune:" + fortune
+        fortune_sentence = "Your fortune: " + fortune
         fortune_paragraph = "<p>" + fortune_sentence + "<p>"
 
         lucky_number = "<strong>" + str(random.randint(1,100)) + "</strong>"
-        number_sentence = "Your lucky number:" + lucky_number
+        number_sentence = "Your lucky number: " + lucky_number
         number_paragraph = "<p>" + number_sentence + "</p>"
 
         cookie_again_button = "<a href='.'><button>Another Cookie Plz!</button></a>"
